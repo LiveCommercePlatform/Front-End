6ab544a0749b6bd8114f77ca55f398a529d8d341
FatemeMoqadam <92844030+FatemeMoqadam@users.noreply.github.com>
2025-10-30 00:38:20 +0330

    feat(category): specific category Page init

--- FrontEnd/src/app/categories/page.tsx
+++ FrontEnd/src/app/categories/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Card, CardContent } from "@/components/ui/card";
 import {
   FaDesktop,
@@ -7,41 +9,51 @@ import {
   FaVideo,
   FaBook,
 } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const categories = [
   {
     title: "تحلیل کسب و کار",
     description: "توضیحاتی درباره تحلیل کسب و کار و اهمیت آن.",
     icon: <FaChartBar className="w-10 h-10 text-orange-500" />,
+    name: "business-analytics",  // تگ انگلیسی برای URL
   },
   {
     title: "مدیریت و ارتباطات",
     description: "توضیح کوتاهی از این دوره، برخی از مزایا.",
     icon: <FaUsers className="w-10 h-10 text-teal-500" />,
+    name: "management-communication",  // تگ انگلیسی برای URL
   },
   {
     title: "طراحی وب",
     description: "توضیح کوتاهی از این دوره.",
     icon: <FaDesktop className="w-10 h-10 text-blue-500" />,
+    name: "web-design",  // تگ انگلیسی برای URL
   },
   {
     title: "گیت‌هاب",
     description: "توضیح کوتاهی از این دوره.",
     icon: <FaBook className="w-10 h-10 text-green-500" />,
+    name: "github",  // تگ انگلیسی برای URL
   },
   {
     title: "ایجاد ویدیو",
     description: "توضیح کوتاهی از این دوره.",
     icon: <FaVideo className="w-10 h-10 text-yellow-500" />,
+    name: "video-creation",  // تگ انگلیسی برای URL
   },
   {
     title: "عکاسی",
     description: "توضیح کوتاهی از این دوره.",
     icon: <FaCamera className="w-10 h-10 text-pink-500" />,
+    name: "photography",  // تگ انگلیسی برای URL
   },
 ];
 
+
 export default function CategoriesPage() {
+    const router = useRouter();
+
   return (
     <div className="min-h-screen bg-[#faf9f6] py-16 px-6">
       <h1 className="text-4xl md:text-5xl font-bold text-center mb-14">
@@ -67,7 +79,8 @@ export default function CategoriesPage() {
                 {category.description}
               </p>
               <div className="flex justify-end">
-                <button className="text-emerald-600 font-medium hover:underline transition">
+                <button            onClick={() => router.push(`/categories/${category.name}`)}
+ className="text-emerald-600 font-medium hover:underline transition">
                   → مشاهده
                 </button>
               </div>
